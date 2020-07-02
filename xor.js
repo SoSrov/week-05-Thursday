267b8d37552eba1c2dc6dbe763be64be34fd2d71
Sopheak Srov <sosrov@gmail.com>
2020-07-02 19:40:10 -0400

    XOR code function

--- xor.js
+++ xor.js
@@ -1,7 +1,17 @@
 function xor(cond1, cond2) {
 	// if cond1 is true and cond2 is false return true
+	if (cond1 && !cond2) {
+		return true
+	} else if (!cond1 && cond2) {
+		return true
+	}
+	else { return false }
 
 	// if cond1 is false and cond2 is true return true
 
+
 	// return false
+
+
+
 }
