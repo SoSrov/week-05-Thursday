9d4104e93ada4143fcc1290081a59749836f900b
Sopheak Srov <sosrov@gmail.com>
2020-07-02 20:17:59 -0400

    For Looping

--- all.js
+++ all.js
@@ -1,4 +1,15 @@
+const anArray = [true, false]
 function all(arr) {
+	for (let x = 0; x < arr.length; x++) {
+		if (!arr[x]) {
+			return false
+		} else if (x >= arr.length) {
+			return true
+		}
+		return arr
+
+	}
+	console.log(all(anArray));
 	// Loop through every item in the array
 	// If the item evaluates to false, return false
 
